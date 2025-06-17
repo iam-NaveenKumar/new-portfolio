1f953a606153446378d00ea52b73124c31b675ad
iam-NaveenKumar <naveen8220258309@gmail.com>
2025-06-17 17:16:39 +0530

    commited

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/new-portfolio/', // 👈 change this!
   plugins: [react()],
 })
